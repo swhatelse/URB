3289edd2720a510ac72fc68274a37d1c2da7fca0
swhatelse <stevenwhatelse@hotmail.fr>
2015-12-22 20:36:46 +0100

    U test

--- src/tests/test_client.c
+++ src/tests/test_client.c
@@ -1,17 +1,32 @@
+#include<getopt.h>
 #include"../client.h"
 
 int main(int argc, char** argv){
-  char buf[8];
-  int sfd[3];
+    int opt;
+    char* config;
+    char buf[8];
+    int sfd[3];
   
-  for(int i = 0; i < 3; i++){ 
-    sfd[i] = connection("127.0.0.1");
-    printf("%d\n", sfd[i]);
-  }
+    if(argc < 2){
+        perror("Argument missing");
+        exit(EXIT_FAILURE);
+    }
   
-  recv(sfd[0], buf, sizeof(buf), 0);
+    while( (opt = getopt(argc, argv, "f:") ) != -1){
+        switch(opt){
+        case 'f':
+            config = optarg;
+            break;
+        default:
+            exit(EXIT_FAILURE);
+        }
+    }
 
-  /* PRINT(buf); */
+    join(config);
+  
+    recv(sfd[0], buf, sizeof(buf), 0);
 
-  return EXIT_SUCCESS;
+    /* PRINT(buf); */
+
+    return EXIT_SUCCESS;
 }
--- src/tests/test_server.c
+++ src/tests/test_server.c
@@ -1,7 +1,25 @@
+#include<getopt.h>
 #include"../server.h"
 
 int main(int argc, char** argv){
-  connexion_init();
-  connexion_handler();
-  return EXIT_SUCCESS;
+    int opt;
+    int port;
+    if(argc < 2){
+        perror("Argument missing");
+        exit(EXIT_FAILURE);
+    }
+  
+    while( (opt = getopt(argc, argv, "p:") ) != -1){
+        switch(opt){
+        case 'p':
+            port = atoi(optarg);
+            break;
+        default:
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    connexion_init(port);
+    connexion_handler();
+    return EXIT_SUCCESS;
 }

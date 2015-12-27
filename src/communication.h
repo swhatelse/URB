997f9c91cb7fb4b901c1d199e2895cb71ec0e7a0
swhatelse <stevenwhatelse@hotmail.fr>
2015-12-27 21:03:30 +0100

    Added new .h

---
+++ src/communication.h
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+#include"common.h"
+
+#ifndef COMMUNICATION
+#define COMMUNICATION
+
+void beb(void *message);
+void urb(void* message);
+#endif
---
+++ src/failure_detector.h
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+#include"common.h"
+
+#ifndef FAILURE_DETECTOR
+#define FAILURE_DETECTOR
+
+void init();
+int check();
+void* run();
+
+#endif

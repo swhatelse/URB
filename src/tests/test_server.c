bad13a77633d1e6a18670c48f65ae9a000ef699a
swhatelse <stevenwhatelse@hotmail.fr>
2015-12-29 20:35:29 +0100

    U test_server

--- src/tests/test_server.c
+++ src/tests/test_server.c
@@ -4,21 +4,27 @@
 int main(int argc, char** argv){
     int opt;
     int port;
+    char* hostfile = NULL;
+    
     if(argc < 2){
         perror("Argument missing");
         exit(EXIT_FAILURE);
     }
   
-    while( (opt = getopt(argc, argv, "p:") ) != -1){
+    while( (opt = getopt(argc, argv, "p:f:") ) != -1){
         switch(opt){
         case 'p':
             port = atoi(optarg);
+            break;
+        case 'f':
+            hostfile = optarg;
             break;
         default:
             exit(EXIT_FAILURE);
         }
     }
 
+    init(hostfile, "127.0.0.1", port);
     connexion_init(port);
     connexion_handler();
     return EXIT_SUCCESS;

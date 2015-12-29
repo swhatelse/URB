bd7372e4f04b4a5adb6fe2d07f23b0b90f64ab29
swhatelse <stevenwhatelse@hotmail.fr>
2015-12-29 20:37:32 +0100

    Managing connexion and msg

--- src/server.c
+++ src/server.c
@@ -1,22 +1,43 @@
 #include<assert.h>
+#include<sys/select.h>
+#include<sys/types.h>
+#include<unistd.h>
+#include<sys/time.h>
 
 #include"server.h"
 
+/********************************************
+ *
+ *             Macros
+ *
+ *******************************************/
 #define BACKLOG 50
 
-int sfd; // listening socket for connexion
-group_t reception_sockets;
-node_t *first_node = NULL;
-
+/********************************************
+ *
+ *             Global vars
+ *
+ *******************************************/
+int listening_fd; // listening socket for connexion
+group_t reception_sockets; // Use for incomming messages
+fd_set reception_fd_set; // File descriptor to watch
+/********************************************
+ *
+ *             Functions
+ *
+ *******************************************/
+/** Prepare the socket for waiting connexion
+ *
+ */
 void connexion_init(){
     struct sockaddr_in my_addr;
     // TODO here just for the moment
-    reception_sockets.nodes = NULL;
+    /* reception_sockets.nodes = NULL; */
 
     PRINT("Initialization of the listener");
     
-    sfd = socket(AF_INET, SOCK_STREAM,0);
-    if(sfd < 0){
+    listening_fd = socket(AF_INET, SOCK_STREAM,0);
+    if(listening_fd < 0){
         perror("Failed to attribute the socket\n");
         exit(EXIT_FAILURE);
     }
@@ -25,21 +46,54 @@ void connexion_init(){
     my_addr.sin_port = htons(my_port);
     my_addr.sin_addr.s_addr = INADDR_ANY;
 
-    if(bind(sfd, (struct sockaddr*) &my_addr, sizeof(my_addr)) < 0 ){
+    if(bind(listening_fd, (struct sockaddr*) &my_addr, sizeof(my_addr)) < 0 ){
         perror("Failed to name the socket\n");
         exit(EXIT_FAILURE);    
     }
 
-    if(listen(sfd, BACKLOG) < 0){
+    if(listen(listening_fd, BACKLOG) < 0){
         perror("Failed to listen\n");
         exit(EXIT_FAILURE);    
     }
 }
 
 void* connexion_handler(){
+    struct timeval timeout;
+    int event = 0;
+    timeout.tv_sec = 5;
+    timeout.tv_usec = 0;
+    
     PRINT("Start to listen");
     while(1){
-        connexion_accept();
+        // Need to reinitialize the set each time
+        FD_ZERO(&reception_fd_set);
+        // Re-fill the set
+        FD_SET(listening_fd, &reception_fd_set);
+        for(int i = 0; i < receive_sockets.count; i++){
+            if(receive_sockets.nodes[i].fd != 0){
+                FD_SET(receive_sockets.nodes[i].fd, &reception_fd_set);
+            }
+        }
+
+        
+        event = select(FD_SETSIZE, &reception_fd_set, NULL, NULL, &timeout);
+        if(event == -1){
+            
+        }
+        else if(event){
+            if(FD_ISSET(listening_fd, &reception_fd_set)){
+                connexion_accept();
+            }
+            
+            for(int i = 0; i < receive_sockets.count; i++){
+                if(FD_ISSET(receive_sockets.nodes[i].fd, &reception_fd_set)){
+
+                }
+            }
+        }
+        else{
+
+        }
     }
     return NULL;
 }
@@ -49,12 +103,14 @@ void* connexion_handler(){
  * @param addr Address information of the incomming connexion
  * @return 0 if ok 1 if fails
  */
-int add_node(const int fd, struct sockaddr_in addr){
-    assert(fd > 2);
+int add_node(const int fd, const struct sockaddr_in addr){
+    assert(fd > 2); // To be sure the fd is valid
+    
     for(int i = 0; i < receive_sockets.count; i++){
         if(receive_sockets.nodes[i].infos.sin_addr.s_addr == 0){
             receive_sockets.nodes[i].infos = addr;
             receive_sockets.nodes[i].fd = fd;
+            FD_SET(fd, &reception_fd_set);
             return EXIT_SUCCESS;
         }
     }
@@ -67,17 +123,18 @@ int connexion_accept(){
     struct sockaddr_in peer_addr;
     socklen_t peer_addr_size;
   
-    cfd = accept(sfd, (struct sockaddr*) &peer_addr, &peer_addr_size);
+    cfd = accept(listening_fd, (struct sockaddr*) &peer_addr, &peer_addr_size);
     if(cfd < 0){
         perror("Failed to accept\n");
         exit(EXIT_FAILURE);    
     }
-    
+
+    // TODO: Need to detect if the group is full before adding
     add_node(cfd, peer_addr);
     
-    sprintf(buf, "client %d %d is connected", peer_addr.sin_addr.s_addr, peer_addr.sin_port);
+    sprintf(buf, "client from %d %d is connected", peer_addr.sin_addr.s_addr, peer_addr.sin_port);
     PRINT(buf);
-
+    
     return cfd;
 }
 

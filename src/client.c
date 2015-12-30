fad5bb885ba8b9412198a15d71f7ec24fb7f1ec8
swhatelse <stevenwhatelse@hotmail.fr>
2015-12-30 17:38:39 +0100

    Clean up client

--- src/client.c
+++ src/client.c
@@ -1,9 +1,6 @@
 #include<string.h>
 #include"client.h"
 
-/* #define PRINT(x)                                \ */
-    /* printf("Client : %s\n", x) */
-
 int connection(const char* addr, const int port){
     int sfd;
     struct sockaddr_in srv_addr;
@@ -28,54 +25,9 @@ int connection(const char* addr, const int port){
         return -1;    
     }
   
-    /* close(sfd); */
     return sfd;
 }
 
-// TODO: modify the join
-/* int join(char* fname){ */
-/*     char buf[16]; */
-/*     char** group = NULL; */
-/*     int group_count = 0; */
-/*     FILE *fd; */
-  
-/*     fd = fopen(fname, "r"); */
-  
-/*     while(fgets(buf, sizeof(buf), fd) != NULL){ */
-/*         group = realloc( (void*) group, group_count + 1 ); */
-/*         group[group_count] = malloc(16); */
-/*         memcpy(group[group_count], buf, strlen(buf)); */
-/*         group_count++; */
-/*     } */
-
-/*     // Debug to remove */
-/*     for(int i = 0; i < group_count; i++){     */
-/*         printf("%s", group[i]); */
-/*     } */
-
-/*     for(int i = 0; i < group_count; i++){ */
-/*         char *addr; */
-/*         int port; */
-/*         char sep = ':'; */
-        
-/*         addr = strtok(group[i], &sep); */
-/*         port = atoi(strtok(NULL, &sep)); */
-/*         printf("%d\n", port); */
-/*         connection(addr, port); */
-/*     } */
-  
-/*     for(int i = 0; i < group_count; i++){ */
-/*         free(group[i]); */
-/*     } */
-/*     free(group); */
-  
-/*     fclose(fd); */
-    
-/*     return EXIT_SUCCESS; */
-/* } */
-
-/** Attempt to connect the server to all the servers in the group
- */
 void join(){
     int *fds = NULL;
     char buf[64];

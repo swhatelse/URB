dd31e0d28d130d13954688b828d2573b7dc0351d
swhatelse <stevenwhatelse@hotmail.fr>
2016-01-07 17:46:37 +0100

    Added missing list.h & list.c

---
+++ src/list.c
@@ -0,0 +1,47 @@
+#include"list.h"
+
+void dlk_list_init(dlk_list_t* list){
+    list->head = NULL;
+    list->tail = NULL;
+}
+
+/** Append the element at the end of the list
+ *
+ */
+void dlk_list_append(dlk_list_t* list, dlk_element_t* element){
+    element->prev = list->head;
+    if(element->prev){
+        element->prev->next = element;
+    }
+    element->next = NULL;
+    list->head = element;
+
+    if(!list->tail){
+        list->tail = element;
+    }
+}
+
+/** Unlink the element from the list.
+ *
+ */
+void dlk_list_remove(dlk_list_t* list, dlk_element_t* element){
+    if(element == list->head){
+        list->head = element->prev;
+    }
+
+    if(element == list->tail){
+        list->tail = element->next;
+    }
+    
+    if(element->prev){
+        element->prev->next = element->next;
+    }
+
+    if(element->next){
+        element->next->prev = element->prev;
+    }
+    
+    element->prev = NULL;
+    element->next = NULL;
+}
+
---
+++ src/list.h
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+#ifndef LIST_H
+#define LIST_H
+
+typedef struct dlk_element_t{
+    void* data;
+    struct dlk_element_t* prev;
+    struct dlk_element_t* next;
+}dlk_element_t;
+
+typedef struct dlk_list_t{
+    dlk_element_t* head;
+    dlk_element_t* tail;
+}dlk_list_t;
+
+void dlk_list_init(dlk_list_t* list);
+void dlk_list_append(dlk_list_t* list, dlk_element_t* element);
+void dlk_list_remove(dlk_list_t* list, dlk_element_t* element);
+
+#endif

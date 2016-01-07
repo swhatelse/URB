97bd47d1e12f28601a26c1c7f36ff81af998abcb
swhatelse <stevenwhatelse@hotmail.fr>
2016-01-07 23:10:12 +0100

    Added debug color

--- src/common.h
+++ src/common.h
@@ -21,21 +21,36 @@
  *****************************************/
 #define RED "\e[31m"
 #define GREEN "\e[32m"
+#define YELLOW "\e[33m"
+#define BLUE "\e[34m"
+#define CYAN "\e[96m"
 #define RESET "\e[m"
 
 #define PRINT(string)                           \
     printf("%d: %s\n" , my_port, string)
 
-#define DEBUG_ERR(fmt, ...)                                                    \
+#define DEBUG_ERR(fmt, ...)                                             \
     do                                                                  \
     {                                                                   \
-    fprintf(stderr, RED fmt RESET , ##__VA_ARGS__);                     \
+        fprintf(stderr, RED fmt RESET , ##__VA_ARGS__);                 \
     }while(0);
 
-#define DEBUG_VALID(fmt, ...)                                                  \
+#define DEBUG_VALID(fmt, ...)                                           \
     do                                                                  \
     {                                                                   \
-    fprintf(stderr, GREEN fmt RESET , ##__VA_ARGS__);                   \
+        fprintf(stderr, GREEN fmt RESET , ##__VA_ARGS__);               \
+    }while(0);
+
+#define DEBUG_RECV(fmt, ...)                                            \
+    do                                                                  \
+    {                                                                   \
+        fprintf(stderr, CYAN fmt RESET , ##__VA_ARGS__);                \
+    }while(0);
+
+#define DEBUG_SEND(fmt, ...)                                            \
+    do                                                                  \
+    {                                                                   \
+        fprintf(stderr, YELLOW fmt RESET , ##__VA_ARGS__);                \
     }while(0);
 
 #define DEBUG(fmt, ...)                                                 \
